25a952dea1e30f04eae7f1bf3b6af0d01e16fce1
Vinicius-Madeira <viniciussmadeira@gmail.com>
2023-07-12 15:56:24 -0300

    Teste 01

---
+++ Testes/Program.cs
@@ -0,0 +1,12 @@
+﻿// Teste 01: Exibir média do Aluno
+
+float nota_1, nota_2, media;
+
+Console.WriteLine("Digite a nota do primeiro semestre:");
+nota_1 = float.Parse(Console.ReadLine());
+Console.WriteLine("Digite a nota do segundo semestre:");
+nota_2 = float.Parse(Console.ReadLine());
+
+media = (nota_1 + nota_2) / 2;
+
+Console.WriteLine($"Sua média é de {media}");
